81903a4a8cef4cc479b60b8367601cffe13d9c2d
taKana671 <kanae5321@gmail.com>
2021-09-20 12:13:20 +0900

    Added new class.

--- pytetris.py
+++ pytetris.py
@@ -1,9 +1,50 @@
 import pygame
 import sys
+from collections import namedtuple
 from pygame.locals import *
 
-
 SCREEN = Rect(0, 0, 700, 500)
+BLOCK_AREA_LEFT = 100
+BLOCK_AREA_RIGHT = 350
+BLOCK_AREA_BOTTOM = 470
+
+
+Shaped = namedtuple('Shaped', 'filename coordinates')
+BLUE = Shaped('images/red25.png', [[225, 100], [225, 125], [225, 150], [225, 175]])
+
+
+class LineShaped:
+
+    def __init__(self):
+        self.blocks = [block for block in self.initialize()]
+        self.sort_blocks()
+
+    def initialize(self):
+        for x, y in BLUE.coordinates:
+            yield Block(BLUE.filename, x, y)
+
+    def sort_blocks(self):
+        self.blocks.sort(key=lambda block: block.y, reverse=True)
+        max_y = self.blocks[0].y
+        self.bottoms = [block for block in self.blocks if block.y == max_y]
+        self.tops = [block for block in self.blocks if block.y != max_y]
+
+    def move_right(self):
+        for block in self.blocks:
+            block.move_right()
+
+    def move_left(self):
+        for block in self.blocks:
+            block.move_left()
+
+    def move_down(self):
+        for bottom_block in self.bottoms:
+            bottom_block.move_down()
+        for top_block in self.tops:
+            if bottom_block.stop:
+                top_block.stop = True
+            else:
+                top_block.move_down()
 
 
 class Block(pygame.sprite.Sprite):
@@ -15,20 +56,36 @@ class Block(pygame.sprite.Sprite):
         self.image = pygame.image.load(filename).convert()
         self.rect = self.image.get_rect()
         self.x = x
-        self.y = y  
+        self.y = y
+        self.stop = False
         # self.rect.left = x
         # self.rect.top = y
 
     def update(self):
-        self.y += self.SPEED
+        if self.rect.bottom <= BLOCK_AREA_BOTTOM and not self.stop:
+            self.y += self.SPEED
         self.rect.centerx = self.x
         self.rect.centery = self.y
 
     def move_right(self):
-        if self.rect.right > SCREEN.right:
-            self.x = SCREEN.right
+        if self.rect.right > BLOCK_AREA_RIGHT:
+            self.x = BLOCK_AREA_RIGHT
         else:
             self.x += 25
+
+    def move_left(self):
+        if self.rect.left < BLOCK_AREA_LEFT:
+            self.x = BLOCK_AREA_LEFT
+        else:
+            self.x -= 25
+
+    def move_down(self):
+        if self.rect.bottom > BLOCK_AREA_BOTTOM:
+            self.y = BLOCK_AREA_BOTTOM
+            self.stop = True
+        else:
+            self.y += 25
+
 
 
 def main():
@@ -36,13 +93,11 @@ def main():
     screen = pygame.display.set_mode(SCREEN.size)
     group = pygame.sprite.RenderUpdates()
     blocks = pygame.sprite.Group()
+    # LineShaped.containers = group, blocks
     Block.containers = group, blocks
     clock = pygame.time.Clock()
-
-    is_wait= True
-
-    block = Block('images/red25.png', 300, 0)
-
+    # block = Block('images/red25.png', 300, 0)
+    block = LineShaped()
 
     while True:
         clock.tick(60)
@@ -61,9 +116,10 @@ def main():
             if event.type == KEYDOWN:
                 if event.key == K_RIGHT:
                     block.move_right()
-                    pygame.display.update()
-
-
+                if event.key == K_LEFT:
+                    block.move_left()
+                if event.key == K_DOWN:
+                    block.move_down()
 
 
 

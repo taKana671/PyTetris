90fa37dc274a0ed7a6c672a927018fb9c6355e11
taKana671 <kanae5321@gmail.com>
2021-09-23 23:23:09 +0900

    Modified Block class

--- pytetris.py
+++ pytetris.py
@@ -1,3 +1,5 @@
+import logging
+
 import pygame
 import sys
 from collections import namedtuple
@@ -7,45 +9,68 @@ SCREEN = Rect(0, 0, 700, 600)
 BLOCK_AREA_LEFT = 100
 BLOCK_AREA_RIGHT = 300
 BLOCK_AREA_BOTTOM = 430
+BLOCK_AREA_TOP = 20
+
+COLS = 10   # the number of columns
+ROWS = 20  # the number of rows
+BLOCK_SIZE = 20
 
 
 Shaped = namedtuple('Shaped', 'filename coordinates')
-BLUE = Shaped('images/red25.png', [[200, 20], [200, 40], [200, 60], [200, 80]])
+BLUE = Shaped('images/red25.png', [[0, 4], [1, 4], [2, 4], [3, 4]])
+
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='test.log')
+logger.addHandler(handler)
 
 
 class LineShaped:
 
-    def __init__(self, blocks, plate):
-        self.group = blocks
-        self.plate = plate
-        self.blocks = [block for block in self.initialize()]
+    def __init__(self):
+        # super().__init__(self.containers)
+        # self.block_group = block_group
+        self.blocks = pygame.sprite.Group()
+        self.initialize()
+        # self.blocks = [block for block in self.initialize()]
 
     def initialize(self):
-        for x, y in BLUE.coordinates:
-            block = Block(BLUE.filename, x, y)
-            self.group.add(block)
-            yield block
+        for row, col in BLUE.coordinates:
+            block = Block(BLUE.filename, row, col)
+            self.blocks.add(block)
+            # yield block
             # yield Block(BLUE.filename, x, y)
 
-    def move_right(self):
-        for block in self.blocks:
-            block.move_right()
+    def update(self):
+        if any(block.col >= COLS for block in self.blocks.sprites()):
+            self.move_left()
+        if any(block.col < 0 for block in self.blocks.sprites()):
+            self.move_right()
+        if any(block.row > ROWS for block in self.blocks.sprites()):
+            self.move_up()
+        for block in self.blocks.sprites():
+            block.rect.centerx = BLOCK_AREA_LEFT + block.col * BLOCK_SIZE
+            block.rect.centery = BLOCK_AREA_TOP + block.row * BLOCK_SIZE
+       
+        if all(block.row <= ROWS for block in self.blocks.sprites()):
+            self.move_down()
 
+    def move_right(self):
+        for block in self.blocks.sprites():
+            block.col += 1
+ 
     def move_left(self):
-        for block in self.blocks:
-            block.move_left()
+        for block in self.blocks.sprites():
+            block.col -= 1
 
     def move_down(self):
-        is_collide = pygame.sprite.spritecollideany(self.plate, self.group)
-        coords = [340, 360, 380, 400]
-        for i, block in enumerate(self.blocks):
-            block.move_down(is_collide, coords[i])
+        for block in self.blocks.sprites():
+            block.row += 1
 
-    def stop(self):
-        if pygame.sprite.spritecollideany(self.plate, self.blocks):
-            for block in self.blocks:
-                block.stop()
-
+    def move_up(self):
+        for block in self.blocks.sprites():
+            block.row -= 1
 
     def rotate(self):
         pass
@@ -53,58 +78,20 @@ class LineShaped:
 
 class Block(pygame.sprite.Sprite):
 
-    SPEED = 1
-
-    def __init__(self, filename, x, y):
+    def __init__(self, filename, row, col):
         super().__init__(self.containers)
         self.image = pygame.image.load(filename).convert()
         self.image = pygame.transform.scale(self.image, (20, 20))
         self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
+        self.row = row
+        self.col = col
         self.stop = False
 
-        # self.rect.left = x
-        # self.rect.top = y
 
-    def update(self):
-        # if self.rect.bottom < BLOCK_AREA_BOTTOM and not self.stop:
-        if not self.stop and self.rect.bottom < BLOCK_AREA_BOTTOM:
-            self.y += self.SPEED
-        self.rect.centerx = self.x
-        self.rect.centery = self.y
+    # def update(self):
+    #     self.rect.centerx = BLOCK_AREA_LEFT + self.col * BLOCK_SIZE
+    #     self.rect.centery = BLOCK_AREA_TOP + self.row * BLOCK_SIZE
 
-    def move_right(self):
-        if self.rect.right > BLOCK_AREA_RIGHT:
-            self.x = BLOCK_AREA_RIGHT
-        elif not self.stop:
-            self.x += 20
-
-    def move_left(self):
-        if self.rect.left < BLOCK_AREA_LEFT:
-            self.x = BLOCK_AREA_LEFT
-        elif not self.stop:
-            self.x -= 20
-
-    def move_down(self, is_collide, y):
-        if is_collide:
-            self.stop = True
-            self.y = y
-            # self.y = self.rect.centery
-        elif not self.stop:
-            self.y += 20
-        # if self.rect.bottom >= BLOCK_AREA_BOTTOM:
-        #     self.y = BLOCK_AREA_BOTTOM
-        #     self.stop = True
-        # elif not self.stop:
-        #     self.y += 25
-
-    def stop(self):
-        self.stop = True
-        self.y = self.rect.centery
-
-    def rotate(self):
-        pass
 
 
 class Plate(pygame.sprite.Sprite):
@@ -122,20 +109,38 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN.size)
     group = pygame.sprite.RenderUpdates()
-    blocks = pygame.sprite.Group()
+    # blocks = pygame.sprite.Group()
+
+
+    matrix = [[None for _ in range(COLS)] for _ in range(ROWS)]
+
     # Block.containers = group, blocks
     Block.containers = group
     Plate.containers = group
-    clock = pygame.time.Clock()
-    # block = Block('images/red25.png', 300, 0)
+    # LineShaped.containers = group
+
     plate = Plate('images/plate.png')
-    block = LineShaped(blocks, plate)
-    # block = LineShaped()
+    # block_group = pygame.sprite.Group()
+
+    # for row, col in BLUE.coordinates:
+    #     block = Block(BLUE.filename, row, col)
+    #     block_group.add(block)
+
+    # for s in block_group.sprites():
+    #     logger.info(f'{s.__dict__}')
+
+    tetris = LineShaped()
+
+    clock = pygame.time.Clock()
+    
 
     while True:
-        clock.tick(60)
+        clock.tick(5)
         # pygame.key.set_repeat(10)
         screen.fill((0, 100, 0))
+        
+        tetris.update()
+
         group.update()
         group.draw(screen)
 
@@ -147,20 +152,16 @@ def main():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_RIGHT:
-                    block.move_right()
+                    tetris.move_right()
                 if event.key == K_LEFT:
-                    block.move_left()
+                    tetris.move_left()
                 if event.key == K_DOWN:
-                    block.move_down()
+                    tetris.move_down()
                 if event.key == K_UP:
-                    # block.rorate()
+                    # tetris.rorate()
                     pass
 
-        if pygame.sprite.spritecollideany(plate, blocks):
-            for b in block.blocks:
-                b.stop = True
-
-        
+     
 
 
 if __name__ == '__main__':

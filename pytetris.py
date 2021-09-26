f39cae6ae614a7462cec64988b7a8fb810b714f8
taKana671 <kanae5321@gmail.com>
2021-09-26 12:18:18 +0900

    Modified to use namedtuple.

--- pytetris.py
+++ pytetris.py
@@ -1,6 +1,7 @@
 import logging
 
 import pygame
+import random
 import sys
 from collections import namedtuple
 from pygame.locals import *
@@ -16,9 +17,28 @@ ROWS = 20  # the number of rows
 BLOCK_SIZE = 20
 
 
-Shaped = namedtuple('Shaped', 'filename coordinates')
-BLUE = Shaped('images/red25.png', [[-1, 4], [0, 4], [1, 4], [2, 4]])
+Assembled = namedtuple('Assembled', 'filename coordinates')
 
+
+class AssembledBlocks(Assembled):
+
+    def update_coords(self):
+        pass
+
+
+
+
+
+BLUE = Shaped('images/blue25.png',
+              [[[-1, 4], [0, 4], [1, 4], [2, 4]], [[0, 4], [0, 5], [0, 6], [0, 7]], [[-1, 4], [0, 4], [1, 4], [2, 4]], [[0, 4], [0, 5], [0, 6], [0, 7]]])
+DARK = Shaped('images/dark25.png', [[-1, 4], [0, 4], [1, 4], [1, 5]])
+GREEN = Shaped('images/green25.png', [[-1, 4], [-1, 5], [0, 3], [0, 4]])
+ORANGE = Shaped('images/orange25.png', [[-1, 4], [-1, 5], [0, 5], [1, 5]])
+PURPLE = Shaped('images/purple25.png', [[-1, 4], [0, 3], [0, 4], [0, 5]])
+RED = Shaped('images/red25.png', [[-1, 3], [-1, 4], [0, 4], [0, 5]])
+YELLOW = Shaped('images/yellow25.png', [[-1, 4], [-1, 5], [0, 4], [0, 5]])
+# SHAPED_LIST = [BLUE, DARK, GREEN, ORANGE, PURPLE, RED, YELLOW]
+SHAPED_LIST = [BLUE]
 
 logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
@@ -35,16 +55,14 @@ class PyTetris:
         self.start()
 
     def start(self):
+        index = random.randint(0, len(SHAPED_LIST) - 1)
+        shaped = SHAPED_LIST[index]
         self.blocks = pygame.sprite.Group()
-        for row, col in BLUE.coordinates:
-            block = Block(BLUE.filename, row, col)
+        for row, col in shaped.coordinates:
+            block = Block(shaped.filename, row, col)
             self.blocks.add(block)
 
     def update(self):
-        # Game over
-        if any(block for block in self.matrix[0]):
-            self.stop = True
-            self.game_over()
         if not self.stop:
             self.move_down()
         if any(self.judge_right(block) for block in self.blocks.sprites()):
@@ -55,10 +73,16 @@ class PyTetris:
             self.move_up()
         if any(self.judge_ground(block) for block in self.blocks.sprites()):
             self.update_matrix()
-            self.start()
-        for block in self.blocks.sprites():
-            block.rect.centerx = BLOCK_AREA_LEFT + block.col * BLOCK_SIZE
-            block.rect.centery = BLOCK_AREA_TOP + block.row * BLOCK_SIZE
+            # Game over
+            if any(block for block in self.matrix[0]):
+                self.stop = True
+                self.game_over()
+            else:
+                self.start()
+        if not self.stop:
+            for block in self.blocks.sprites():
+                block.rect.centerx = BLOCK_AREA_LEFT + block.col * BLOCK_SIZE
+                block.rect.centery = BLOCK_AREA_TOP + block.row * BLOCK_SIZE
 
     def judge_left(self, block):
         return block.col < 0 or self.matrix[block.row][block.col]
@@ -94,6 +118,7 @@ class PyTetris:
             block.row -= 1
 
     def rotate(self):
+
         pass
 
     def game_over(self):
@@ -131,14 +156,10 @@ def main():
     group = pygame.sprite.RenderUpdates()
 
     matrix = [[None for _ in range(COLS)] for _ in range(ROWS)]
-
-    # Block.containers = group, blocks
     Block.containers = group
     Plate.containers = group
-    # LineShaped.containers = group
 
     plate = Plate('images/plate.png')
-    # block_group = pygame.sprite.Group()
 
     tetris = PyTetris(matrix, screen)
 
